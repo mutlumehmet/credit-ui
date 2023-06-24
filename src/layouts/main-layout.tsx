2335f0bb84e76302a314b6b6a03d78034360e6d2
Mehmet Mutlu <mcrmehmet@gmail.com>
2023-06-24 14:48:04 +0300

    feat: Datagrid customizations
    Signed-off-by: Mehmet Mutlu <mcrmehmet@gmail.com>

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,8 +1,17 @@
-import { DataGrid } from "devextreme-react/data-grid";
 import notify from "devextreme/ui/notify";
 import { RawData } from "../types";
 import { useLayoutEffect, useState } from "react";
 import getValidatedData from "../helper/data-valdiator";
+import DataGrid, {
+  SearchPanel,
+  Item as GridItem,
+  Toolbar,
+  Paging,
+  FilterRow,
+} from "devextreme-react/data-grid";
+import Button from "devextreme-react/button";
+
+import { CellPreparedEvent, Column } from "devextreme/ui/data_grid";
 
 export default function MainLayout() {
   const [data, setData] = useState<RawData[]>();
@@ -22,5 +31,28 @@ export default function MainLayout() {
     handleDataFetch();
   }, []);
 
-  return <DataGrid dataSource={data} />;
+  return (
+    <div className="flex flex-col justify-center items-center mt-14 px-20">
+      <DataGrid
+        className="h-[80vh]"
+        dataSource={data}
+        keyExpr="companyName"
+        showBorders={true}
+        allowColumnResizing={true}
+        columnResizingMode={"widget"}
+        allowColumnReordering={true}
+        rowAlternationEnabled={true}
+      >
+        <Paging enabled={false} />
+        <Toolbar>
+          <GridItem location="before" locateInMenu="false">
+            <Button text="Refresh" icon="refresh" onClick={handleDataFetch} />
+          </GridItem>
+          <GridItem location="after" locateInMenu="false" name="searchPanel" />
+        </Toolbar>
+        <FilterRow visible={true} />
+        <SearchPanel visible={true} />
+      </DataGrid>
+    </div>
+  );
 }
