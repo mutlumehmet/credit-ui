673e531fa325badf3e3d265bc054253dcde77907
Mehmet Mutlu <mcrmehmet@gmail.com>
2023-06-24 13:12:53 +0300

    feat: App initialized
    Signed-off-by: Mehmet Mutlu <mcrmehmet@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header className="App-header flex justify-center">
+        <div className="bg-black rounded-lg p-4 text-white">header </div>
+      </header>
+      <div className="bg-white rounded-lg p-4 text-black flex justify-center">
+        main layout{" "}
+      </div>
+    </div>
+  );
+}
+
+export default App;
